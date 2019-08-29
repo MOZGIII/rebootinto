99c067b27b0e58bc0984ea317c7f461c89ff239a
MOZGIII <mike-n@narod.ru>
2019-08-29 06:43:48 +0300

    Updated tui and crossterm

--- tui/src/terminal_backend/crossterm.rs
+++ tui/src/terminal_backend/crossterm.rs
@@ -1,8 +1,11 @@
 use crate::Result;
+
+use std::io;
+
 pub use tui::backend::CrosstermBackend;
 
-pub fn create_terminal_backend() -> Result<CrosstermBackend> {
+pub fn create_terminal_backend() -> Result<CrosstermBackend<io::Stdout>> {
     let alternate_screen = crossterm::AlternateScreen::to_alternate(true)?;
-    let backend = CrosstermBackend::with_alternate_screen(alternate_screen)?;
+    let backend = CrosstermBackend::with_alternate_screen(io::stdout(), alternate_screen)?;
     Ok(backend)
 }
