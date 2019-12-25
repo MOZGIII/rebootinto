c823ea89850ccb566bfbd22ceb821d495c54dda2
MOZGIII <mike-n@narod.ru>
2019-12-25 19:35:14 +0300

    Switched to alternate screen wrapper for crossterm

--- tui/src/terminal_backend/crossterm.rs
+++ tui/src/terminal_backend/crossterm.rs
@@ -3,38 +3,54 @@ use crate::Result;
 use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    Command, ErrorKind,
+    ErrorKind,
 };
-use std::io::{self, Write};
+use std::io;
 use tui::backend::CrosstermBackend;
 
 pub struct Crossterm {
-    inner: CrosstermBackend<io::Stdout>,
+    inner: CrosstermBackend<AlternateScreenWriter<io::Stdout>>,
 }
 
 impl Backend for Crossterm {
     fn new() -> Result<Self> {
-        enable_raw_mode()?;
-        let mut stdout = io::stdout();
-        typed_execute(&mut stdout, EnterAlternateScreen)?;
+        let stdout = io::stdout();
+        let stdout = AlternateScreenWriter::new(stdout)?;
         let inner = CrosstermBackend::new(stdout);
         let backend = Self { inner };
         Ok(backend)
     }
 }
 
-impl Drop for Crossterm {
+delegate_backend_impl!(Crossterm, self => self.inner);
+
+struct AlternateScreenWriter<W: std::io::Write>(W);
+
+impl<W: std::io::Write> AlternateScreenWriter<W> {
+    pub fn new(mut w: W) -> std::result::Result<Self, ErrorKind> {
+        enable_raw_mode()?;
+        execute!(&mut w, EnterAlternateScreen)?;
+        Ok(Self(w))
+    }
+}
+
+impl<W: std::io::Write> Drop for AlternateScreenWriter<W> {
     fn drop(&mut self) {
-        typed_execute(&mut self.inner, LeaveAlternateScreen).unwrap();
+        execute!(&mut self.0, LeaveAlternateScreen).unwrap();
         disable_raw_mode().unwrap();
     }
 }
 
-delegate_backend_impl!(Crossterm, self => self.inner);
+impl<W: std::io::Write> std::io::Write for AlternateScreenWriter<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        std::io::Write::write(&mut self.0, buf)
+    }
 
-fn typed_execute<W: Write, C: Command>(
-    write: &mut W,
-    command: C,
-) -> std::result::Result<(), ErrorKind> {
-    execute!(write, command)
+    fn write_vectored(&mut self, bufs: &[std::io::IoSlice<'_>]) -> std::io::Result<usize> {
+        std::io::Write::write_vectored(&mut self.0, bufs)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        std::io::Write::flush(&mut self.0)
+    }
 }
