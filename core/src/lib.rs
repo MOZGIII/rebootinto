ee19799bc74ba38a058a4c2c3763297d93b2520f
MOZGIII <mike-n@narod.ru>
2020-01-26 11:10:27 +0300

    Switched to the efibootnext 0.2

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,9 +3,7 @@
 #[macro_use]
 extern crate failure_derive;
 
-use efibootnext;
-use efivar;
-
+use efibootnext::Adapter;
 pub use efibootnext::LoadOption;
 pub use failure::Error;
 
@@ -14,22 +12,23 @@ mod error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub struct Backend {
-    var_manager: Box<dyn efivar::VarManager>,
+    adapter: Adapter,
 }
 
 impl Backend {
     pub fn init() -> Result<Self> {
         Ok(Self {
-            var_manager: efivar::system(),
+            adapter: Adapter::default(),
         })
     }
 
     pub fn load_options<'a>(&'a mut self) -> impl Iterator<Item = Result<LoadOption>> + 'a {
-        efibootnext::load_options(self.var_manager.as_mut())
+        self.adapter.load_options()
     }
 
     pub fn reboot_into(&mut self, num: u16) -> Result<()> {
-        efibootnext::set_boot_next(self.var_manager.as_mut(), num)
+        self.adapter
+            .set_boot_next(num)
             .map_err(error::RebootIntoErrorKind::SetBootNextError)?;
         simplereboot::reboot()
             .map_err(|e| Error::from(e))
