7d25dd38eda70c757ddd9408e6b7871efb1c92c8
MOZGIII <mike-n@narod.ru>
2022-12-27 04:52:00 +0400

    Update to modern wix at CI

--- .github/scripts/packaging_env/windows.sh
+++ .github/scripts/packaging_env/windows.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # Build upon the build environment.
 .github/scripts/build_env/windows.sh
 
-choco install wixtoolset
-cargo install -f cargo-wix
+dotnet tool install --global wix --version 4.0.0-rc.1
+wix extension add --global WixToolset.UI.wixext/4.0.0-rc.1
