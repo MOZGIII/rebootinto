1defedda4d57f94a4bc425867c6125840c3c325a
MOZGIII <mike-n@narod.ru>
2022-12-31 15:00:01 +0400

    Install fake cargo package at CI (#107)

--- .github/scripts/packaging_env/linux.sh
+++ .github/scripts/packaging_env/linux.sh
@@ -9,6 +9,16 @@ sudo apt-get install -y \
   equivs \
   devscripts
 
+(
+  EQUIVS_TMP="$(mktemp -d)"
+  trap 'rm -rf "$EQUIVS_TMP"' EXIT
+
+  REPO_ROOT="$(pwd)"
+  cd "$EQUIVS_TMP"
+  equivs-build "$REPO_ROOT/.github/scripts/packaging_env/linux/equivs/cargo"
+  sudo dpkg -i cargo_1.0_all.deb
+)
+
 mk-build-deps \
   --install \
   --root-cmd sudo \
