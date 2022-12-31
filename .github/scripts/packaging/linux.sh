f547d641b8402db0fa8d16943c6e60487dc126f3
MOZGIII <mike-n@narod.ru>
2022-12-31 13:09:53 +0400

    Install fake cargo package at CI

--- .github/scripts/packaging/linux.sh
+++ .github/scripts/packaging/linux.sh
@@ -3,9 +3,14 @@ set -euo pipefail
 
 ARTIFACTS_DIR="$1"
 
+CARGO_PATH="$(dirname "$(which cargo)")"
+
 # TODO: do not build the code again
 gbp buildpackage \
   --git-ignore-branch \
+  --git-builder=debuild \
+  --prepend-path "$CARGO_PATH" \
+  -i -I \
   --no-sign \
   --no-pre-clean \
   --no-check-builddeps \
